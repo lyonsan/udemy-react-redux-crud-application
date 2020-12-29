ecd53580c30f7474bd240090de0b72eda5583fbc
lyonsan <ryoga.yajima@gmail.com>
2020-12-29 21:51:20 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+/*class App extends Component {
   render() {
     return (
       //returnで返すjsxは一つのタグでなければならないというルールがある
@@ -11,6 +11,21 @@ class App extends Component {
     )
     
   }
+}*/
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
