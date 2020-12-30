ebecdbb203ece2705a675c3bac907424cdb9bb3f
lyonsan <ryoga.yajima@gmail.com>
2020-12-30 13:15:01 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const increment = () => ({
+  type: DECREMENT
+})
+
