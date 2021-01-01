949d39b5baee2c83242db3692c41834a9e5345f7
lyonsan <ryoga.yajima@gmail.com>
2021-01-01 11:40:57 +0900

    apply material-ui to eventsIndex

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash'
 import { Link } from 'react-router-dom'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 import { readEvents } from '../actions';
 
 
@@ -12,36 +22,49 @@ class EventsIndex extends Component {
 
   renderEvents() {
     return _.map(this.props.events, event => (
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableRowColumn>{event.id}</TableRowColumn>
+        <TableRowColumn>
           {/* 詳細ページにリンクを飛ばすようにする。このURLで表示されるのはEventsShowコンポーネント */}
           <Link to={`/events/${event.id}`} >
             {event.title}
           </Link>
-        </td>
-        <td>{event.body}</td>
-      </tr>
+        </TableRowColumn>
+        <TableRowColumn>{event.body}</TableRowColumn>
+      </TableRow>
     ))
   }
 
   render () {
+    const style = {
+      position: "fixed",
+      right: 12,
+      bottom: 12
+    }
     return (
       <React.Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
+        {/* 新規作成ボタンを作っている。ContentAddで＋の文字、styleで表示の位置を変更している */}
+        <FloatingActionButton style={style} containerElement={<Link to="/events/new" />}>
+          <ContentAdd />
+        </FloatingActionButton>
+        <Table>
+          <TableHeader
+            //既存の属性をセットしてチェックボックスを表示させなくする
+            displaySelectAll={false}
+            adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
             {this.renderEvents()}
-          </tbody>
-        </table>
+          </TableBody>
+        </Table>
 
-        <Link to="/events/new">New Event</Link>
+        
       </React.Fragment>
         
     )
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers';
@@ -21,18 +22,21 @@ const enhancer = process.env.NODE_ENV ==='development' ?
 const store = createStore(reducer, enhancer )
 
 ReactDOM.render(
-  <Provider store={store}>
-    {/* リンクをevent_index.jsファイルにて指定しており、Provider内でリンクを有効にするためにwrapする必要がある */}
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        {/* 様々なidをとるため:をつける */}
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  //見た目を整えるためにMaterialUIを使うが、MuiThemeProviderでラップすることで、ラップした範囲内で使えるようになる
+  <MuiThemeProvider>
+    <Provider store={store}>
+      {/* リンクをevent_index.jsファイルにて指定しており、Provider内でリンクを有効にするためにwrapする必要がある */}
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          {/* 様々なidをとるため:をつける */}
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
   );
 registerServiceWorker();
